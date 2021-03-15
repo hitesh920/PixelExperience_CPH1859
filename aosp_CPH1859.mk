8de506b913502c5155b8ba3f2978490b56d96ff6
hitesh920 <hiteshkamble920@gmail.com>
2021-03-15 11:33:57 +0000

    CPH1859: Intial pixel experince Bringup

--- aosp_CPH1859.mk
+++ aosp_CPH1859.mk
@@ -19,8 +19,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixel Experience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from oppo CPH1861
 $(call inherit-product, device/oppo/CPH1859/device.mk)
@@ -28,13 +28,16 @@ $(call inherit-product, device/oppo/CPH1859/device.mk)
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# PixelExperience specifics
+TARGET_GAPPS_ARCH=arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Bootanimation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_CPH1859
+PRODUCT_NAME := aosp_CPH1859
 PRODUCT_DEVICE := CPH1859
 PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BRAND := OPPO
@@ -50,4 +53,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="CPH1859" \
     PRIVATE_BUILD_DESC="full_oppo6771_17065-user 9 PPR1.180610.011 eng.root.20200509.060522 release-keys"
 
-BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ2A.210305.006/7119741:user/release-keys"
+BUILD_FINGERPRINT := "google/flame/flame:S/SPP1.210122.022/7158812:user/release-keys"
